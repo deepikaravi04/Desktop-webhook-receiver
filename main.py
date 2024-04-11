a824075ba8b25f02472ceae7d0e2dca78ccb4b4f
rocket <deepikaraviup@gmail.com>
2024-04-11 16:51:12 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from pyngrok import ngrok
+
+# Set up a HTTP tunnel on port 8000
+public_url = ngrok.connect(8000, "http")
+
+print("Public URL:", public_url)
+
+# Keep the tunnel open until interrupted
+try:
+    input("Press Enter to exit...")
+except KeyboardInterrupt:
+    ngrok.disconnect(public_url)
